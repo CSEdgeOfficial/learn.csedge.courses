ab56d5d4e5ced88f5baf4211883ae829624ba135
KavyaBS123 <kavyabsalawadagi@gmail.com>
2024-06-06 17:21:58 +0530

    added social media icons

--- ScrollToTop.js
+++ ScrollToTop.js
@@ -1,3 +1,19 @@
+(function () {
+    let socials = document.querySelectorAll('.social div')
+        
+    socials.forEach(function(social, index) {
+      social.style.animation = `moveIn .6s cubic-bezier(.51,.92,.24,1.15) forwards ${index/7 + 0.2}s`
+    })})
+
+
+
+
+
+
+
+
+
+
 const goTopBtn=document.querySelector('.go-top-btn')
 
 window.addEventListener('scroll',checkHeight)
