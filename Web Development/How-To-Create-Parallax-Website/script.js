e44f813428b91e1503e5c4bf715aa9c14482a753
Sai pradyumna Goud Chiragoni <143107589+Saipradyumnagoud@users.noreply.github.com>
2024-06-27 21:00:16 +0530

    added How to create a parallax website

---
+++ Web Development/How-To-Create-Parallax-Website/script.js
@@ -0,0 +1,10 @@
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
